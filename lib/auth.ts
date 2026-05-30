1e0880a02b97501b7b0fba8b733a8b9a986ab5f0
Dima-salang <luisgabrielle1026@gmail.com>
2026-05-30 22:04:20 +0800

    add auth with better auth

---
+++ lib/auth.ts
@@ -0,0 +1,25 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db, isSQLite } from "@/db";
+import * as schema from "@/db/schema";
+import { organization } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    // Dynamically toggle dialect for development/production
+    provider: isSQLite ? "sqlite" : "pg",
+    schema: schema,
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID || "placeholder",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "placeholder",
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID || "placeholder",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "placeholder",
+    },
+  },
+  plugins: [organization()],
+});
+export type Auth = typeof auth;
