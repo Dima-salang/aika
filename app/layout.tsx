3abcd187c1eea25c2b5b7a117c3afa7e9c706eaf
Dima-salang <luisgabrielle1026@gmail.com>
2026-06-22 23:32:52 +0800

    removed jetbrains mono font

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
             `,
           }}
         />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=JetBrains+Mono:wght@500&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet"/>
       </head>
       <body className="min-h-screen flex flex-col w-full bg-background text-foreground antialiased font-sans">
